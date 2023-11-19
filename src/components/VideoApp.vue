8e3da9bb7750429eb7a2056b1e9b86f6f87ce7ac
Markelas <markgirfanov@gmail.com>
2023-11-20 00:30:19 +0500

    Change link

--- src/components/VideoApp.vue
+++ src/components/VideoApp.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="video__box">
-    <video src="https://file.notion.so/f/f/9e6aa08e-de3b-4fa2-a7ab-a0ac08e1ddea/43e05f39-570a-41d2-84dc-692c2d68b83e/video1687347191.mp4?id=fa4f026c-95ab-4170-a481-55dbcddfb059&table=block&spaceId=9e6aa08e-de3b-4fa2-a7ab-a0ac08e1ddea&expirationTimestamp=1700215200000&signature=prg5z8dfADrNTqgqHX1SWmoPNTeISVG_vTuF-u94SS4&downloadName=video1687347191.mp4" width="390" class="video__player" controls @click="textStore.isToggleVideo"></video>
+    <video src="https://file.notion.so/f/f/9e6aa08e-de3b-4fa2-a7ab-a0ac08e1ddea/43e05f39-570a-41d2-84dc-692c2d68b83e/video1687347191.mp4?id=fa4f026c-95ab-4170-a481-55dbcddfb059&table=block&spaceId=9e6aa08e-de3b-4fa2-a7ab-a0ac08e1ddea&expirationTimestamp=1700510400000&signature=9jFagzqyMl9jyujr147uGjnVHUBJS7Tw8d5R8op8BK0" width="390" class="video__player" controls @click="textStore.isToggleVideo"></video>
   </div>
 </template>
 
